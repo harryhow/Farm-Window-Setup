4df887e785424fab887a4bf71c78f26f71e2e0d5
Harry Chen <harryhow@gmail.com>
2016-06-03 19:46:32 -0400

    ui components

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,17 +6,29 @@ void ofApp::setup(){
     ofSetVerticalSync(true);
     ofSetWindowShape(width, height);
     gui.setup(); // most of the time you don't need a name
-    gui.add(choose.setup("", ""));
     gui.add(mediaCenter.setup("FW Media Center", false));
     gui.add(fwplayer.setup("FW Player", true));
-
-    gui.setPosition(100, 250);
-    gui.setDefaultWidth(550);
+    gui.setPosition(60, 250);
+    
     
     myfont32.loadFont("FZLTZCHJW.ttf", 32);
     myfont24.loadFont("FZLTZCHJW.ttf", 24);
     myfont16.loadFont("FZLTXHJW.ttf", 16);
+    
 
+    rect.set(350, 100, 400, 200);
+    
+    // Transformations applied to the rectangle
+    dx = 100; dy = 450;          // A translation...
+    scaleX = 0.5; scaleY = 0.3; // ... followed by a scale ...
+    angle = 0;                 // ... followed by a rotation
+    
+    // All the transformations in one matrix
+    matrix.glTranslate( dx, dy, 0 );
+    matrix.glRotate( angle, 0, 0, 1 );
+    matrix.glScale( scaleX, scaleY, 1 );
+    
+    isInside = false;
 }
 
 //--------------------------------------------------------------
@@ -27,6 +39,7 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
     
+        
     ofBackgroundGradient(ofColor::white, ofColor::gray);
     gui.draw();
     
@@ -47,6 +60,50 @@ void ofApp::draw(){
     
     ofSetColor(ofColor::darkSlateGrey);
     
+    if (mediaCenter || fwplayer) {
+        
+        isProceedClickable = true;
+    }
+    else
+        isProceedClickable = false;
+    
+    
+    ofPushMatrix();
+        ofMultMatrix(matrix);
+        // Same as :
+        // ofTranslate( dx, dy, 0 );
+        // ofRotate( angle, 0, 0, 1 );
+        // ofScale( scaleX, scaleY, 1 );
+    
+    
+    
+        if(isInside){
+            ofSetColor(ofColor::darkSlateGrey, 100);
+            ofFill();
+        }
+        else{
+            ofSetColor(ofColor::darkSlateGrey);
+            ofFill();
+        }
+    
+        if (!isProceedClickable){
+            ofSetColor(ofColor::darkSlateGrey, 100);
+            ofNoFill();
+        }
+    
+        ofRect(rect);
+    ofPopMatrix();
+    
+    
+    ofSetColor(ofColor::white);
+    myfont16.drawString("Install",  345, 515);
+    ofSetColor(ofColor::darkSlateGrey);
+    
+   
+}
+
+void ofApp::proceedBtnPressed() {
+
 }
 
 //--------------------------------------------------------------
@@ -69,6 +126,10 @@ void ofApp::keyReleased(int key){
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
+    
+    ofVec3f mouse( x, y, 0 );
+    mouse = mouse * matrix.getInverse();
+    isInside = rect.inside( mouse );
 
 }
 
@@ -79,11 +140,20 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
+    
+    //cout << "mouse pressed, x: " << x << "y:" << y;
+//    bool clicked;
+//    if (button == 0) {
+//        clicked = !clicked;
+//        cout << clicked << endl;
+//    }
 
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
+    
+    //cout << "mouse release, x: " << x << "y:" << y;
 
 }
 
@@ -106,5 +176,6 @@ void ofApp::gotMessage(ofMessage msg){
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
 
 }
+
 
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -3,7 +3,6 @@
 #include "ofMain.h"
 #include "ofxGui.h"
 
-
 class ofApp : public ofBaseApp{
 
 	public:
@@ -27,13 +26,25 @@ class ofApp : public ofBaseApp{
     
         //ofxUI setup
         ofxPanel gui;
+        ofxPanel guiBtn;
     	ofxToggle mediaCenter;
         ofxToggle fwplayer;
-        ofxLabel choose;
+        ofxButton proceed;
     
         ofTrueTypeFont myfont24;
         ofTrueTypeFont myfont32;
         ofTrueTypeFont myfont16;
     
+        void proceedBtnPressed();
+        bool isProceedClickable;
+    
+        // button rectangle
+        float top, bottom, left, right;
+        ofRectangle rect;
+        float dx, dy;
+        float scaleX, scaleY;
+        float angle;
+        ofMatrix4x4 matrix;
+        bool isInside;
     
 };
